2d029a75ed5bb5585f6a77e900014f72cbdbb860
Yurii Popleta <yurka279@gmail.com>
2022-08-06 15:23:46 +0200

    Initial Commit

---
+++ H4X0R News/H4X0R_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_NewsApp.swift
+//  H4X0R News
+//
+//  Created by Admin on 06/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
